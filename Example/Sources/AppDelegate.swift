3316560afdc511c6b0369d1c148292ede7d513f5
Dan Trenz <dtrenz@gmail.com>
2017-01-06 14:08:52 -0500

    Updated to Swift v3

--- Example/Sources/AppDelegate.swift
+++ Example/Sources/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     /**
     *  Configure BugShaker with an array of email recipients (required)
     *  and an optional custom subject line to use for all bug reports.
